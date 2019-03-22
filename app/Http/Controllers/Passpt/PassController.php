db67d32d609cb5d88580a968037ce38962cb20eb
supreme <supreme@localhost.localdomain>
2019-03-22 02:38:42 +0000

    aaa

---
+++ app/Http/Controllers/Passpt/PassController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Passpt;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Model\UserModel;
+use Illuminate\Support\Facades\Redis;
+
+class PassController extends Controller
+{
+    //
+
+    public function passPortll(){
+        $data=$_POST;
+        $where=[
+            'user_name'=>$data['user_name']
+        ];
+        var_dump($data);exit;
+        $model=UserModel::where($where)->first();
+        if($model){
+            $pwd=pssword_verify($data['user_password'],$model['user_pwd']);
+            if($pwd==true){
+                $token=substr(md5(time().mt_rand(1,99999)),10,10);
+                $key="str:u:token:api:".$model['user_id'];
+                Redis::set($key,$token);
+                $response=[
+                    'errno'=>0,
+                    'token'=>$token
+                ];
+            }else{
+                $response=[
+                    'errno'=>4000002,
+                    'msg'=>'登录失败'
+                ];
+            }
+        }else{
+            $response=[
+                'srrno'=>400003,
+                'msg'=>'用户名有误'
+            ];
+        }
+        return $response;
+    }
+
+}
---
+++ app/Http/Middleware/checkLoginToken.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class checkLoginToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(isset($_COOKIE['uid']) && isset($_COOKIE['token'])){
+            //验证token
+            $key='str:u:token:web:'.$_COOKIE['uid'];
+
+            $token=Redis::get($key);
+            if($_COOKIE['token']==$token){
+                //token有效
+                $request->attributes->add(['is_login'=>1]);
+            }else{
+                //token无效
+                $request->attributes->add(['is_login'=>0]);
+            }
+        }else{
+            //未登录
+            $request->attributes->add(['is_login'=>0]);
+        }
+//        if(!$request->session()->get('u_token')){
+//            echo json_encode([
+//                'error' => 301,
+//                'url'   => url('/user/login')
+//            ]);
+//            die;
+//        }
+        return $next($request);
+    }
+}
