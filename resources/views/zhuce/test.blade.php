5d82a4414fd905ed47878f60034b8fd95dbe11ce
supreme <supreme@localhost.localdomain>
2019-03-22 04:03:09 +0000

    kkkk

--- resources/views/zhuce/test.blade.php
+++ resources/views/zhuce/test.blade.php
@@ -8,14 +8,6 @@
         <h2 class="form-signin-heading">用户注册</h2>
         <label for="inputNickName">Nickname</label>
         <input type="text" name="nick_name" id="inputNickName" class="form-control" placeholder="nickname" required autofocus>
-
-        <label for="inputAge">age</label>
-        <input type="text" name="age" id="inputAge" class="form-control" placeholder="nickname" required autofocus>
-
-
-        <label for="inputEmail">Email</label>
-        <input type="email" name="u_email" id="inputEmail" class="form-control" placeholder="@" required autofocus>
-
         <label for="inputPassword" >密码</label>
         <input type="password" name="u_pass" id="inputPassword" class="form-control" placeholder="***" required>
 
